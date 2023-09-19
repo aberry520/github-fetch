c6e3f03b6abdeb1484824464a6f68bf45365b4c9
Austin Berry <aberry52098@gmail.com>
2023-09-19 12:12:31 -0400

    add get function

---
+++ js/get.js
@@ -0,0 +1,9 @@
+async function get(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
